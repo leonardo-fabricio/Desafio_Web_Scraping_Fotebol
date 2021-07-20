9e82ddc7465f4fab7874e9da5a17363caf1a0511
leonardo-fabricio <leonardo.fabricio@escolar.ifrn.edu.br>
2021-07-20 10:44:00 -0300

    Algumas implementações

--- Projeto2/app/views.py
+++ Projeto2/app/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
+from .models import Time
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    aux = None
+    serieA = Time.objects.filter(serie='A')
+    serieB = Time.objects.filter(serie='B')
+
+    context = {
+        'serieA': serieA,
+        'serieB': serieB
+    }
+    return render(request,'index.html', context)
 
 def cadastro(request):
     return render(request,'cadastro.html')
+
+# def product_new(request):
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST)
+#         if form.is_valid():
+#             product = Product()
+#             product.user = request.user
+#             product.name = form.cleaned_data['name']
+#             product.quantity = form.cleaned_data['quantity']
+#             product.price = form.cleaned_data['price']
+#             product.short_description = form.cleaned_data['short_description']
+#             product.description = form.cleaned_data['description']
+#             product.status = 'Active'
+#             product.save()
+
+#             categories = Category.objects.filter(id__in=request.POST.getlist('categories'))
+#             if categories:
+#                 for category in categories:
+#                     product.categories.add(category)
+#             return redirect('my_products')
+
+
+#     form = ProductForm()
+
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'portal/product_new.html', context)
